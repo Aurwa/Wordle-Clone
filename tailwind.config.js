a6d6d63aa38f488cc7909ead627f38cce8b9b3e1
Aurwa Zafar <zafaraurwa11@gmail.com>
2023-08-16 23:15:05 +0500

    update UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.html", "./*.js"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['"Open Sans"'],
+    },
+    extend: {
+      colors: {
+        bodyBg: "#121213",
+        darkendYellow: "#b59f3b",
+        correctColor: "#538d4e",
+        absentColor: "#3a3a3c",
+      },
+    },
   },
   plugins: [],
 };
